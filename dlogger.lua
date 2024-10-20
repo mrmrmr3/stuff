49bbf41201644a070b5ac2e07fbcbdb64b714d5c
mrmrmr3 <82025245+mrmrmr3@users.noreply.github.com>
2024-10-20 14:16:38 +0700

    Update dlogger.lua

--- dlogger.lua
+++ dlogger.lua
@@ -24,17 +24,9 @@ local RoomsData: Folder = Instance.new("Folder")
 RoomsData.Parent = Leader
 RoomsData.Name = "_ROOMS_DATA"
 
-local Keys: Folder = Instance.new("Folder")
-Keys.Name = "Keys"
-Keys.Parent = RoomsData
-
 local Misc: Folder = Instance.new("Folder")
 Misc.Name = "Misc"
 Misc.Parent = RoomsData
-
-local Levers: Folder = Instance.new("Folder")
-Levers.Name = "Levers"
-Levers.Parent = RoomsData
 
 local Info: Folder = Instance.new("Folder")
 Info.Name = "Info"
@@ -43,7 +35,7 @@ Info.Parent = RoomsData
 -- Main
 
 local function DeepClone(Object: Instance, Folder: Folder)
-	Object.Destroying:Connect(function()
+	task.delay(0.5, function()
 		local Object1: Instance = Object:Clone()
 		Object1.Parent = Folder or Misc
 	end)
@@ -60,28 +52,12 @@ end
 local function DescendantAdded(Desc: BasePart | Instance)
 	local Attributes: {[string]: any} = Desc:GetAttributes()
 	local Module: string = Attributes.LoadModule
-	
-	if Module == "KeyObtain" or Attributes.IsKey then
-		DeepClone(Desc, Keys)
-	elseif (Module == "TimerLever" or Desc.Name == "TimerLever") or Module == "AffectGate" then
-		DeepClone(Desc, Levers)
-	elseif Desc:IsA("BasePart") then
+
+	if Desc:IsA("BasePart") then
 		if Desc.CollisionGroup == "BaseCheck" then
 			DeepClone(Desc, Info)
 			Desc:SetAttribute("ROOM", findRoom(Desc):GetAttribute("Raw_Name"))
 		end
-	elseif Desc:IsA("BoolValue") and Desc.Name == "NearElevator" then
-		Desc:GetPropertyChangedSignal("Value"):Connect(function()
-			if Desc.Value == true then
-				local Figure: Model = workspace:FindFirstChild("Figure", true)
-				local ElevatorCar: Model = workspace:FindFirstChild("ElevatorCar", true)
-				local Distance: Vector3 = (Figure:GetPivot().Position - ElevatorCar:GetPivot().Position)
-				
-				warn("[X] NearElevator.Value |", Distance.Magnitude, "|", Distance)
-				
-				RoomsData:SetAttribute("NearElevator", Distance)
-			end
-		end)
 	elseif Desc:IsA("Folder") then
 		if Desc.Name == "PathfindNodes" then
 			DeepClone(Desc, Info)
@@ -92,8 +68,6 @@ end
 local function RoomAdded(Room: Model)
 	if Room:IsA("Model") then
 		Room.DescendantAdded:Connect(DescendantAdded)
-	else
-		warn("[X] Foreign Instance", Room)
 	end
 end
 
@@ -102,17 +76,17 @@ workspace.CurrentRooms.ChildAdded:Connect(RoomAdded)
 local function WorkspaceAdded(Object: Instance)
 	if Object:IsA("Model") and Object.Name == "Repentance" then
 		local Time = os.clock()
-		
+
 		task.spawn(function()
 			while true do
-				if (os.clock() - Time) >= 20 then
+				if (os.clock() - Time) >= 30 then
 					break
 				end
-				
+
 				local Crux: Model = Object:Clone()
 				Crux.Parent = Repentance
-				
-				task.wait(0.1)
+
+				task.wait(0.5)
 			end
 		end)
 	end
@@ -123,10 +97,11 @@ workspace.ChildAdded:Connect(WorkspaceAdded)
 local function AmbientAdded(Attachment: Attachment)
 	task.spawn(function()
 		if Attachment:IsA("Attachment") then
-			Attachment.Destroying:Connect(function()
+			task.delay(0.5, function()
 				local Ambience: Sound = Attachment:FindFirstChildOfClass("Sound"):Clone()
-				Ambience.Parent = Sounds
 				
+				Ambience.Parent = Sounds
+
 				pcall(function()
 					Ambience:Stop()
 					Ambience.TimePosition = 0
