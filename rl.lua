b974460eae8528106bf39d4a900618497cff98c5
mrmrmr3 <82025245+mrmrmr3@users.noreply.github.com>
2024-10-20 14:13:30 +0700

    Update rl.lua

--- rl.lua
+++ rl.lua
@@ -25,54 +25,54 @@ makefolder(fpmain)
 game.ReplicatedStorage.GameData.LatestRoom:GetPropertyChangedSignal("Value"):Connect(function()
 	task.spawn(function()
 		local nr = workspace.CurrentRooms:WaitForChild(tostring(game.ReplicatedStorage.GameData.LatestRoom.Value) + 1)
-		
+
 		task.delay(1, function()
 			local n = nr:FindFirstChild("PathfindNodes")
-			
+
 			if n then
 				n = n:Clone()
 				n.Name = "PathfindNodes"
-				
+
 				task.delay(0.5, function()
 					n.Parent = nr
 				end)
 			end
 		end)
 	end)
-	
+
 	local lr = workspace.CurrentRooms:FindFirstChild(tostring(game.ReplicatedStorage.GameData.LatestRoom.Value))
-	
+
 	if lr then
-		local fn = lr:GetAttribute("RawName") .. " - [" .. ts() .. "]"
+		local fn = tostring(lr.Name) .. " - " .. lr:GetAttribute("RawName") .. " - [" .. ts() .. "]"
 		local fp = fpmain .. "/" .. fn
-		
+
 		local o = {}
 
 		o.timeout = 16384
 		o.noscripts = true
 		o.mode = "invalid"
 		o.ReadMe = false
-		
+
 		o.FilePath = fp
 		o.Object = lr
-		
+
 		if o.Object then
 			dec(o)
-			
+
 			task.spawn(function()
 				if not isfile("_siderooms") then
 					makefolder("_siderooms")
 				end
-				
+
 				if not isfile("_siderooms/" .. fl) then
 					makefolder("_siderooms/" .. fl)
 				end
-				
+
 				for _, sr in lr:GetChildren() do
 					if sr.Name == "Sideroom" then
 						local siz = tostring(sr:GetAttribute("Weight"))
 						local bv = sr:GetExtentsSize()
-						
+
 						if bv.X > 50 and bv.Z > 50 then
 							siz = "Big"
 						elseif bv.Y > 40 then
@@ -82,7 +82,7 @@ game.ReplicatedStorage.GameData.LatestRoom:GetPropertyChangedSignal("Value"):Con
 						elseif bv.Z > 50 then
 							siz = "Wide"
 						end
-						
+
 						local fn2 = "Sideroom" .. siz .. " - [" .. ts() .. "]"
 						local fp2 = "_siderooms/" .. fl .. "/" .. fn2
 						local o2 = {}
@@ -94,7 +94,7 @@ game.ReplicatedStorage.GameData.LatestRoom:GetPropertyChangedSignal("Value"):Con
 
 						o2.FilePath = fp2
 						o2.Object = lr
-						
+
 						dec(o2)
 					end
 				end
