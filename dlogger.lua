9774f0c70ce39a8ad4475219326e4489312563a7
mrmrmr3 <82025245+mrmrmr3@users.noreply.github.com>
2024-10-22 09:59:30 +0700

    Update dlogger.lua

--- dlogger.lua
+++ dlogger.lua
@@ -32,7 +32,44 @@ local Info: Folder = Instance.new("Folder")
 Info.Name = "Info"
 Info.Parent = RoomsData
 
+local NewAttributes: Folder = Instance.new("Folder")
+NewAttributes.Name = "NewAttributes"
+NewAttributes.Parent = RoomsData
+
 -- Main
+
+local attributesIndex = {}
+local function newAttribute(Name: string)
+	if table.find(attributesIndex, Name) then
+		return
+	end
+	
+	table.insert(attributesIndex, Name)
+	
+	local na = Instance.new("StringValue")
+	na.Name = Name
+	na.Parent = NewAttributes
+	na:SetAttribute("Drops", #workspace.Drops:GetChildren())
+	na:SetAttribute("Number", game.ReplicatedStorage.GameData.LatestRoom.Value)
+	
+	return na
+end
+
+game.Players.LocalPlayer.AttributeChanged:Connect(function(Name: string)
+	local na = newAttribute(Name)
+
+	if na then
+		na.Value = tostring(game.Players.LocalPlayer:GetAttribute(Name))
+	end
+end)
+
+game.Players.LocalPlayer.Character.AttributeChanged:Connect(function(Name: string)
+	local na = newAttribute(Name)
+
+	if na then
+		na.Value = tostring(game.Players.LocalPlayer.Character:GetAttribute(Name))
+	end
+end)
 
 local function DeepClone(Object: Instance, Folder: Folder)
 	task.delay(0.5, function()
@@ -55,8 +92,8 @@ local function DescendantAdded(Desc: BasePart | Instance)
 
 	if Desc:IsA("BasePart") then
 		if Desc.CollisionGroup == "BaseCheck" then
-			DeepClone(Desc, Info)
 			Desc:SetAttribute("ROOM", findRoom(Desc):GetAttribute("Raw_Name"))
+			DeepClone(Desc, Info)
 		end
 	elseif Desc:IsA("Folder") then
 		if Desc.Name == "PathfindNodes" then
@@ -74,21 +111,35 @@ end
 workspace.CurrentRooms.ChildAdded:Connect(RoomAdded)
 
 local function WorkspaceAdded(Object: Instance)
-	if Object:IsA("Model") and Object.Name == "Repentance" then
-		local Time = os.clock()
+	if Object:IsA("Model") then
+		if Object.Name == "Repentance" then
+			local Time = os.clock()
 
-		task.spawn(function()
-			while true do
-				if (os.clock() - Time) >= 30 then
-					break
+			task.spawn(function()
+				while true do
+					if (os.clock() - Time) >= 30 or not Object.Parent then
+						break
+					end
+
+					local Crux: Model = Object:Clone()
+					Crux.Parent = Repentance
+
+					task.wait(0.5)
 				end
-
-				local Crux: Model = Object:Clone()
-				Crux.Parent = Repentance
-
-				task.wait(0.5)
-			end
-		end)
+			end)
+		elseif string.find(string.lower(Object.Name), "mobble") or string.find(string.lower(Object.Name), "jack") then
+			Object.Destroying:Once(function()
+				local new = Object:Clone()
+				Object.Parent = EntityData
+			end)
+			
+			task.delay(0.1, function()
+				if Object then
+					local new = Object:Clone()
+					Object.Parent = EntityData
+				end
+			end)
+		end
 	end
 end
 
@@ -98,14 +149,24 @@ local function AmbientAdded(Attachment: Attachment)
 	task.spawn(function()
 		if Attachment:IsA("Attachment") then
 			task.delay(0.5, function()
-				local Ambience: Sound = Attachment:FindFirstChildOfClass("Sound"):Clone()
+				local Sound: Sound = Attachment:FindFirstChildOfClass("Sound")
 				
-				Ambience.Parent = Sounds
+				if Sound then
+					for _, S: Sound in Sounds:GetChildren() do
+						if S.SoundId == Sound.SoundId then
+							return
+						end
+					end
+					
+					local Ambience: Sound = Sound:Clone()
 
-				pcall(function()
-					Ambience:Stop()
-					Ambience.TimePosition = 0
-				end)
+					Ambience.Parent = Sounds
+
+					pcall(function()
+						Ambience:Stop()
+						Ambience.TimePosition = 0
+					end)
+				end
 			end)
 		end
 	end)
